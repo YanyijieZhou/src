8ddd0fb8229a476e7c95aa32b38312185bf97b69
Yanyijie Zhou <themistes@outlook.com>
2025-11-02 15:26:18 -0600

    test.py modified, nothing important

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append("../src")
 import os
 os.getcwd()
-from src import create_project as pf
+from src.my_py import create_project as pf
 
 
 pf.load_template('meeting')
@@ -13,4 +13,4 @@ pf.list_projects()
 
 pf.new_file()
 
-from src import getmy as gm
+from src.my_py import getmy as gm
